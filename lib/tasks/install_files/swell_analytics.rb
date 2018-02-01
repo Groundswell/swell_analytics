2c258c966626d5f674ce7598af713f15af892048
Michael <meister@spacekace.com>
2018-02-01 11:31:38 -0800

    add initializer to install files

---
+++ lib/tasks/install_files/swell_analytics.rb
@@ -0,0 +1,8 @@
+
+SwellAnalytics.configure do |config|
+
+	# config.event_worker_class_name = "SwellAnalytics::EventWorker"
+	# config.async_event_logging = defined?( Sidekiq::Worker )
+	# config.event_worker_options = { :queue => :medium }
+
+end
