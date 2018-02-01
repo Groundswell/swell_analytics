bcf879e598fe4e1051cb8866c973358c97a2c261
Michael <meister@spacekace.com>
2018-02-01 15:24:12 -0800

    track landing page data regardless

--- app/helpers/swell_analytics/application_helper.rb
+++ app/helpers/swell_analytics/application_helper.rb
@@ -16,6 +16,8 @@ module SwellAnalytics
 				campaign_term: params[:utm_term],
 				campaign_content: params[:utm_content],
 				campaign_name: params[:utm_campaign],
+				landing_page_referrer_url: request.referrer,
+				landing_page_page_url: request.original_url,
 			}.merge( options )
 
 
