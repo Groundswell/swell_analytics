863e51576a2c83e5813ca073969ec10653c4b0f9
Michael <meister@spacekace.com>
2018-02-01 15:09:34 -0800

    fix worker

--- app/workers/swell_analytics/event_worker.rb
+++ app/workers/swell_analytics/event_worker.rb
@@ -1,26 +1,29 @@
-class EventWorker
-	if defined?( Sidekiq::Worker )
-		include Sidekiq::Worker
-		sidekiq_options( SwellAnalytics.event_worker_options ) if SwellAnalytics.event_worker_options.present?
-	end
+module SwellAnalytics
+	class EventWorker
+		if defined?( Sidekiq::Worker )
+			include Sidekiq::Worker
+			sidekiq_options( SwellAnalytics.event_worker_options ) if SwellAnalytics.event_worker_options.present?
+		end
 
 
-	def perform( name, options )
-		options = JSON.parse options, symbolize_keys: true
+		def perform( name, options )
+			options = JSON.parse( options, symbolize_names: true )
 
-		# Process Event
-		begin
-
-			AnalyticsService.new.save_event( name, options )
-
-		rescue Exception => e
-			puts e
-
+			# Process Event
 			begin
-				NewRelic::Agent.notice_error( e, custom_params: args )
-			rescue
-				NewRelic::Agent.notice_error( e )
+
+				AnalyticsService.new.save_event( name, options )
+
+			rescue Exception => e
+				puts e
+
+				begin
+					NewRelic::Agent.notice_error( e, custom_params: args )
+				rescue
+					NewRelic::Agent.notice_error( e )
+				end
 			end
+
 		end
 
 	end
