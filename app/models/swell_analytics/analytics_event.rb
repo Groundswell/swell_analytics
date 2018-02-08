8c6b1ee360593abc7daee73ce693ff66e38dc5d3
Michael <meister@spacekace.com>
2018-02-08 13:24:12 -0800

    check version

--- app/models/swell_analytics/analytics_event.rb
+++ app/models/swell_analytics/analytics_event.rb
@@ -2,6 +2,7 @@ module SwellAnalytics
 	class AnalyticsEvent < ActiveRecord::Base
 		self.table_name = 'analytics_events'
 
-		belongs_to :analytics_session, optional: true
+		belongs_to :analytics_session, optional: true if Rails.version.to_i > 4
+		belongs_to :analytics_session if Rails.version.to_i <= 4
 	end
 end
